688fa6067eaee9c3e3cd044ffc62de02f6d68000
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-10-21 17:43:20 -0300

    get and show info from API. Getting the token.

--- lib/app/services/api_services.dart
+++ lib/app/services/api_services.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'api.dart';
@@ -8,6 +9,14 @@ class APIService {
   final API api;
 
   APIService(this.api);
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
 
   Future<String> getAccessToken() async {
     final response = await http.post(
@@ -24,6 +33,33 @@ class APIService {
     }
     print(
       'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}',
+    );
+    throw response;
+  }
+
+  Future<int> getEndPointData({
+    @required String accessToken,
+    @required Endpoint endpoint,
+  }) async {
+    final uri = api.endPointUri(endpoint);
+    final response = await http.get(
+      uri.toString(),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+      'Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}',
     );
     throw response;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -32,12 +32,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _accessToken = '';
+  String _accessToken = 'no token';
+  int _cases;
+  int _deaths;
 
   void _updateAccessToken() async {
     final apiService = APIService(API.sandbox());
     final accessToken = await apiService.getAccessToken();
-    setState(() => _accessToken = accessToken);
+    final cases = await apiService.getEndPointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.cases,
+    );
+    final deaths = await apiService.getEndPointData(
+      accessToken: accessToken,
+      endpoint: Endpoint.deaths,
+    );
+    setState(() {
+      _accessToken = accessToken;
+      _cases = cases;
+      _deaths = deaths;
+    });
   }
 
   @override
@@ -57,6 +71,24 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_accessToken',
               style: Theme.of(context).textTheme.headline4,
             ),
+            if (_cases != null)
+              Text(
+                'Cases',
+              ),
+            if (_cases != null)
+              Text(
+                '$_cases',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            if (_deaths != null)
+              Text(
+                'Deaths',
+              ),
+            if (_deaths != null)
+              Text(
+                '$_deaths',
+                style: Theme.of(context).textTheme.headline4,
+              ),
           ],
         ),
       ),
